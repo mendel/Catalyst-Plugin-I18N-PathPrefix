95d94103c49d94f8a8dcbbb1d2e53aac0e0f7ac3
Norbert Buchmuller <norbi@nix.hu>
2010-09-30 15:24:13 +0200

    Bumped version.

--- lib/Catalyst/Plugin/I18N/PathPrefix.pm
+++ lib/Catalyst/Plugin/I18N/PathPrefix.pm
@@ -21,11 +21,11 @@ Catalyst::Plugin::I18N::PathPrefix - Language prefix in the request path
 
 =head1 VERSION
 
-Version 0.01
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 
 =head1 SYNOPSIS
